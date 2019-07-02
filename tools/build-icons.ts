c5b4dc817c2682a34396d95830520a45bbf786a6
Sherif Elmetainy <sherif.elmetainy@gmail.com>
2019-07-02 09:05:06 +0200

    update icons

--- tools/build-icons.ts
+++ tools/build-icons.ts
@@ -7,108 +7,114 @@ const hostName = 'raw.githubusercontent.com';
 const version = '5.0.10';
 const pathPrefix = `/FortAwesome/Font-Awesome/${version}/advanced-options/raw-svg/solid/`;
 const downloadPath = `../projects/angular-datepicker/src/lib/components/icons`;
-const iconNames = ['calendar-alt', 'home', 'chevron-left', 'chevron-up', 'chevron-down',
- 'chevron-right', 'times', 'clock'];
+const iconNames = [
+  'home',
+  'chevron-left',
+  'chevron-up',
+  'chevron-down',
+  'chevron-right',
+  'times',
+];
 
 function getFile(name: string): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        https.get({
-            hostname: hostName,
-            method: 'GET',
-            path: `${pathPrefix}${name}.svg`,
-            port: 443,
-            protocol: 'https:',
-        }, (response) => {
-            let result = '';
-            console.log(`Download file: https://${hostName}${pathPrefix}${name}.svg : Status ${response.statusCode}`);
-            response.on('data', (chunk) => {
-                result += chunk;
-            });
+  return new Promise<string>((resolve, reject) => {
+    https.get({
+      hostname: hostName,
+      method: 'GET',
+      path: `${pathPrefix}${name}.svg`,
+      port: 443,
+      protocol: 'https:',
+    }, (response) => {
+      let result = '';
+      console.log(`Download file: https://${hostName}${pathPrefix}${name}.svg : Status ${response.statusCode}`);
+      response.on('data', (chunk) => {
+        result += chunk;
+      });
 
-            response.on('end', () => {
-                if (response.statusCode && response.statusCode === 200 ) {
-                    resolve(result);
-                } else {
-                    reject(new Error(`Http Error: ${response.statusMessage}. `
-                        + `Body: ${result}.`));
-                }
-            });
-        });
+      response.on('end', () => {
+        if (response.statusCode && response.statusCode === 200) {
+          resolve(result);
+        } else {
+          reject(new Error(`Http Error: ${response.statusMessage}. `
+            + `Body: ${result}.`));
+        }
+      });
     });
+  });
 }
 
 function writeFile(name: string, data: string): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        fs.writeFile(name, data, (err) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
+  return new Promise<void>((resolve, reject) => {
+    fs.writeFile(name, data, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
     });
+  });
 }
 
 function getIconFileName(name: string): string {
-    return `icon-${name}.component`;
+  return `icon-${name}.component`;
 }
 
 function getIconComponentName(name: string): string {
-    return `Icon${changecase.pascal(name)}Component`;
+  return `Icon${changecase.pascal(name)}Component`;
 }
 
 async function downloadIcon(name: string): Promise<void> {
-    let data = await getFile(name);
-    data = '<span [class]="iconCssClassName">' + data + '</span>';
-    await writeFile(`${downloadPath}/${getIconFileName(name)}.html`, data);
+  let data = await getFile(name);
+  data = '<span [class]="iconCssClassName">' + data + '</span>';
+  await writeFile(`${downloadPath}/${getIconFileName(name)}.html`, data);
 }
 
 async function downloadAllIcons(): Promise<void> {
-    for (const icon of iconNames) {
-        await downloadIcon(icon);
-        await writeComponentFile(icon);
-    }
+  for (const icon of iconNames) {
+    await downloadIcon(icon);
+    await writeComponentFile(icon);
+  }
 }
 
 async function writeIndex(): Promise<void> {
-    let p = `import { Type } from '@angular/core';
+  let p = `import { Type } from '@angular/core';
 
 `;
-    const ar = iconNames.slice(0).sort();
-    for (const icon of ar) {
-        p = p + `import { Icon${changecase.pascal(icon)}Component } from './${getIconFileName(icon)}';
+  const ar = iconNames.slice(0).sort();
+  for (const icon of ar) {
+    p = p + `import { Icon${changecase.pascal(icon)}Component } from './${getIconFileName(icon)}';
 `;
-    }
-    p = p + `
+  }
+  p = p + `
 export const ICON_COMPONENTS: Array<Type<any>> = [
 `;
-    for (const icon of ar) {
-        p += `    ${getIconComponentName(icon)},
+  for (const icon of ar) {
+    p += `    ${getIconComponentName(icon)},
 `;
-    }
-    p = p + `];
+  }
+  p = p + `];
 `;
-    await writeFile(`${downloadPath}/index.ts`, p);
+  await writeFile(`${downloadPath}/index.ts`, p);
 }
 
 async function writeComponentFile(name: string): Promise<void> {
-    const p = `import { Component, Input } from '@angular/core';
+  const p = `import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'cadp-icon-${name}',
-    styleUrls: ['../../styles/icon-styles.scss'],
-    templateUrl: './${getIconFileName(name)}.html',
+  selector: 'cadp-icon-${name}',
+  styleUrls: ['../../styles/icon-styles.scss'],
+  templateUrl: './${getIconFileName(name)}.html',
 })
 export class ${getIconComponentName(name)} {
-    @Input() public iconCssClassName = 'icon-${name}';
+  @Input() public iconCssClassName = 'icon-${name}';
 }
 `;
-    await writeFile(`${downloadPath}/${getIconFileName(name)}.ts`, p);
+  await writeFile(`${downloadPath}/${getIconFileName(name)}.ts`, p);
 }
 
 async function buildIconFiles(): Promise<void> {
-    await downloadAllIcons();
-    await writeIndex();
+  await downloadAllIcons();
+  await writeIndex();
 }
 
 buildIconFiles();
